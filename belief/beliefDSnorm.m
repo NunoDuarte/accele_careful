0cd2723b08510a0dc31e10e199bc4fb24815d34a
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-10-10 17:05:41 +0200

    good classification but dependent on normalized distance

--- belief/beliefDSnorm.m
+++ belief/beliefDSnorm.m
@@ -18,7 +18,7 @@ addpath('../../Khansari/SEDS/GMR_lib')
 %% Belief System for 2 DS
 
 % pick e trajectory
-testX = F{1}; 
+testX = F{4}; 
 
 % remove nonzeros
 testXn(:,1) = nonzeros(testX(:,2));
@@ -113,7 +113,7 @@ b = [b1, b2];
 b1_d = 0;
 b2_d = 0;
 b_d = [b1_d, b2_d];
-epsilon = 0.2; % adaptation rate
+epsilon = 0.5; % adaptation rate
 
 d = 1; %dimension of data
 xT = 0;
@@ -126,41 +126,35 @@ Er = [];
 K = 0; % out many values to average
 for j = 1:length(Data)-K-1   
     ee = [0 0];
-    for i = 1:2
-        
-        outD(j) = (Data(2,j+1)-Data(2,j))/0.02;
-  
-        xd = e2{i}(2)/e2{i}(1);
-        
-        % error (real velocity - desired velocity)
-        ed = abs(outD(j) - xd(:,1));
-        ee(i) = ed;   
-        
-        %Xd(j,i) = xd(:,1)';
-        Xd = [Xd, xd(:,1)'];
-        b_d(i) = epsilon * (ed'*xd(:,1) + (b(i) - 0.5)*norm(xd(:,1), 2)); 
-        
-    end
-    Er = [Er;ee];
-    
-    if abs(outD(j)) > 0.15
-       [b1_d, w] = max(b_d); 
-        if w == 1
-            0;
-        elseif w == 2
-            b_dold = b_d;
-            b_d(1) = b1_d;
-            b_d(2) = b_dold(1);       
+
+    if abs(Data(1,j)) > 0.05
+        for i = 1:2
+
+            outD(j) = abs((Data(2,j+1)-Data(2,j))/(Data(1,j+1)-Data(1,j)));
+
+            % rate of change for careful/not careful
+            xd = abs((e2{i}(2)/e2{i}(1)));
+
+            % error (real velocity - desired velocity)
+            ed = -1*abs(outD(j) - xd(:,1));
+            ee(i) = ed;   
+
+            %Xd(j,i) = xd(:,1)';
+            Xd = [Xd, xd(:,1)'];
+            b_d(i) = epsilon * (ed' + (b(i) - 0.5)*norm(xd(:,1), 2)); 
+
         end
+        Er = [Er;ee];
+
+        B_d = winnertakeall(b, b_d);
+        for i = 1:2
+            b(i) = b(i) + B_d(i)*0.1;
+            b(i) = max(0., min(1., b(i)));
+        end
+        b(2) = 1. - b(1);
+        B = [B; b];   
     end
-        
-    B_d = winnertakeall(b, b_d);
-    for i = 1:2
-        b(i) = b(i) + B_d(i)*0.1;
-        b(i) = max(0., min(1., b(i)));
-    end
-    b(2) = 1. - b(1);
-    B = [B; b];    
+
 end
 
 
