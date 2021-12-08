972b5f096bb55df474e2175094d61e83385f5c8b
Nuno Duarte <nunoduarte.cc.fduarte@gmail.com>
2021-12-08 20:06:11 +0000

    try to find clusters - can not find it

--- clustering.m
+++ clustering.m
@@ -43,6 +43,64 @@ if plotting
     plot3(ploty, plotx, plotz, '.');
 end
 
+%% computing the first and second time derivative
+
+dt = 1/120;
+for i=1:length(E3)
+    tmp = E3{i};
+    tmp_d{i} = diff(tmp,1,2)/dt;
+    tmp_2d{i} = diff(tmp_d{i},1,2)/dt;
+end
+
+for i=1:length(tmp_d)
+    % x
+    xT = E3{i}(:,end);
+    Norm1 = [];
+    for j=1:length(E3{i})
+        dis = xT - E3{i}(:,j);
+        disN = norm(dis,2);
+        Norm1 = [Norm1; disN];
+       
+    end
+    % normalized over distance
+    Norm2 = Norm1/max(Norm1);
+    Emp3Dnorm{i} = [Norm2'];
+    
+    % dx1/dt1
+    Norm1 = [];
+    for j=1:length(tmp_d{i})
+        dis = tmp_d{i}(:,j);
+        disN = norm(dis,2);
+        Norm1 = [Norm1; disN];
+       
+    end
+    d3tmp_d{i} = [Norm1'];
+    
+    % dx2/dt2
+    Norm1 = [];
+    for j=1:length(tmp_2d{i})
+        dis = tmp_2d{i}(:,j);
+        disN = norm(dis,2);
+        Norm1 = [Norm1; disN];
+       
+    end
+    d3tmp_2d{i} = [Norm1'];
+end
+
+[plotx, ploty, plotz] = deal([]);
+for i=1:length(tmp_d)
+    if plotting
+        plotz = [plotz, Emp3Dnorm{i}(1,:)];     
+        plotx = [plotx, [d3tmp_d{i}(1,:),NaN]];
+        ploty = [ploty, [d3tmp_2d{i}(1,:), NaN,NaN]];         
+    end
+end
+if plotting
+    figure(4);
+    plot3(plotx, ploty, plotz, '.');
+end
+hold on;
+
 %% Remove Non Zeros - Full
 
 if plotting
@@ -70,3 +128,64 @@ if plotting
     figure();
     plot3(ploty, plotx, plotz, '.');
 end
+
+%% computing the first and second time derivative
+
+clear tmp_d
+clear tmp_2d
+
+dt = 1/120;
+for i=1:length(F3)
+    tmp = F3{i};
+    tmp_d{i} = diff(tmp,1,2)/dt;
+    tmp_2d{i} = diff(tmp_d{i},1,2)/dt;
+end
+
+for i=1:length(tmp_d)
+    % x
+    xT = F3{i}(:,end);
+    Norm1 = [];
+    for j=1:length(F3{i})
+        dis = xT - F3{i}(:,j);
+        disN = norm(dis,2);
+        Norm1 = [Norm1; disN];
+       
+    end
+    % normalized over distance
+    Norm2 = Norm1/max(Norm1);
+    Fmp3Dnorm{i} = [Norm2'];
+    
+    % dx1/dt1
+    Norm1 = [];
+    for j=1:length(tmp_d{i})
+        dis = tmp_d{i}(:,j);
+        disN = norm(dis,2);
+        Norm1 = [Norm1; disN];
+       
+    end
+    d3tmp_d{i} = [Norm1'];
+    
+    % dx2/dt2
+    Norm1 = [];
+    for j=1:length(tmp_2d{i})
+        dis = tmp_2d{i}(:,j);
+        disN = norm(dis,2);
+        Norm1 = [Norm1; disN];
+       
+    end
+    d3tmp_2d{i} = [Norm1'];
+end
+
+[plotx, ploty, plotz] = deal([]);
+for i=1:length(tmp_d)
+    if plotting
+        plotz = [plotz, Fmp3Dnorm{i}(1,:)];     
+        plotx = [plotx, [d3tmp_d{i}(1,:),NaN]];
+        ploty = [ploty, [d3tmp_2d{i}(1,:), NaN,NaN]];         
+    end
+end
+if plotting
+    figure(4);
+    plot3(plotx, ploty, plotz, '.');
+end
+
