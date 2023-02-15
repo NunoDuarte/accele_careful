8c46c3a32c1e044b80772fa6d8023fa4a6a3dc3a
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2023-02-15 14:07:07 +0000

    add new structure of code to batch classification - same results

--- run_batch_classify.m
+++ run_batch_classify.m
@@ -14,13 +14,12 @@ addpath('../software/Khansari/SEDS/SEDS_lib')
 % Which Person to choose 
 % [E, F] = read('Kunpeng', 'red-cup');
 [Egan, Fgan, Eneu, Fneu] = readIST_22();  
+D = Fgan;
 %readQMUL;
 
 % set correct data frequency
 %samp_freq = 1/30; % for QMUL data
 samp_freq = 1/120; % for EPFL data
-
-F{1} = Fgan{1,1};
 
 %% Load Eigen Vectors from 'param' folder
 
@@ -45,51 +44,78 @@ e2{2} = Vf(:,2);
 
 %% Batch all files
 
-D = size(Egan);
-
-for n=1:D(1)
-    for m=1:D(2)
-
-        clear testXn;
-        clear test3;
-       %% Belief System for 2 DS
-
-        % pick e trajectory
-        testX = Fgan{n,m};
-        if isempty(testX)
+len = size(D);
+for n=1:len(1)
+    for m=1:len(2)
+        
+        demo = D{n,m};
+        if isempty(demo)
             continue
-        end
+        end        
+        %% preprocess data
+        % remove 0s rows and NANs
+        demo = demo(any(demo,2),2:4);        
+        demo = demo(all(~isnan(demo),2),:);    
 
-        % preprocess data
-        testXn = testX(any(testX,2),2:4);          % remove only full rows of 0s
-        testXn = testXn(all(~isnan(testXn),2),:);  % remove rows of NANs  
-        test3{1}(1,:) = testXn(:,1)';
-        test3{1}(2,:) = testXn(:,2)';
-        test3{1}(3,:) = testXn(:,3)'; 
+        % set handover point as origin
+        dis = demo - demo(end,:);
 
-        %% Center the Data in the Origin
+        % compute vector-wise L2-norm
+        Norm1 = vecnorm(dis,2,2);
 
-        for i=1:length(test3)
-            xT = test3{i}(:,end);
-            Norm1 = [];
-            for j=1:length(test3{i})
-                dis = xT - test3{i}(:,j);
-                disN = norm(dis,2);
-                Norm1 = [Norm1; disN];
+        % normalized over distance
+        demo_norm = Norm1/max(Norm1);   % THIS IS IMPORTANT (-1) or not
 
-                % normalized over distance
-                Norm2 = Norm1/max(Norm1);
+        %% calculate velocity
+        % de-noising data (not necessary/optional)
+        tmp = smooth(demo_norm,25); 
 
-                % flip data to have the acceleration phase at the end
-                Norm2 = flip(Norm2);
-                Emp3Dnorm{i} = Norm2';
-            end
-        end
+        % computing the first time derivative
+        tmp_d = diff(tmp,1,1)/samp_freq;
+        
+        %% Save data
+        % set the acceleration back to the origin
+        tmp = tmp - repmat(tmp(1),1,size(tmp,2));
 
-        [~ , ~, Data, index] = preprocess_demos(Emp3Dnorm, samp_freq, 0.0001);
-
-        % flip Data to start at (0,0); 
-        Data = flip(Data')';
+        Data=[];
+        Data = [Data [tmp';tmp_d' 0]];       
+       
+%         % pick e trajectory
+%         testX = Egan{n,m};
+%         if isempty(testX)
+%             continue
+%         end
+% 
+%         % preprocess data
+%         testXn = testX(any(testX,2),2:4);          % remove only full rows of 0s
+%         testXn = testXn(all(~isnan(testXn),2),:);  % remove rows of NANs  
+%         test3{1}(1,:) = testXn(:,1)';
+%         test3{1}(2,:) = testXn(:,2)';
+%         test3{1}(3,:) = testXn(:,3)'; 
+% 
+%         %% Center the Data in the Origin
+% 
+%         for i=1:length(test3)
+%             xT = test3{i}(:,end);
+%             Norm1 = [];
+%             for j=1:length(test3{i})
+%                 dis = xT - test3{i}(:,j);
+%                 disN = norm(dis,2);
+%                 Norm1 = [Norm1; disN];
+% 
+%                 % normalized over distance
+%                 Norm2 = Norm1/max(Norm1);
+% 
+%                 % flip data to have the acceleration phase at the end
+%                 Norm2 = flip(Norm2);
+%                 Emp3Dnorm{i} = Norm2';
+%             end
+%         end
+% 
+%         [~ , ~, Data, index] = preprocess_demos(Emp3Dnorm, samp_freq, 0.0001);
+% 
+%         % flip Data to start at (0,0); 
+%         Data = flip(Data')';
 
         %% Classify motion
         % initial values
