303f998ba2f430d9f818198dd71a7c09d80c34c1
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2023-03-01 18:45:52 +0000

    improve readability of code

--- plot_B_data.m
+++ plot_B_data.m
@@ -1,65 +1,65 @@
 % plot B data
-clear E;
-E = all_BFgan;
+clear V;
+% pick the data
+data = {'all_BEgan'; 'all_BEneu'}; 
 
+% do you want to plot only the true positive cases?
+true_positives = 1;
+% do you want to plot the careful (1) or not careful (0)
+careful = 0;
+
+%% read all the files and plot it
 size_of_traj = [];
 reaction_time = [];
-
-len = size(E);
-for n=1:len(1)
-    for m=1:len(2)
-        if length(E{n,m}) ~= 0
-%             if any(E{n,m} == 1)
-                hold on;
-                size_of_traj = [size_of_traj, length(E{n,m})];
-                plot(E{n,m}, 'b-.');
-                segment = E{n,m};
-                reaction_time = [reaction_time; segment(1:198)'];
-%             end
+for k=1:2
+    V = eval(string(data(k)));
+    for n=1:size(V,1)
+        for m=1:size(V,2)
+            if ~isempty(V{n,m})
+                if true_positives
+                    if any(V{n,m} == careful)
+                        hold on;
+                        size_of_traj = [size_of_traj, length(V{n,m})];
+                        plot(V{n,m}, 'b-.');
+                        segment = V{n,m};
+                        reaction_time = [reaction_time; segment(1:198)'];
+                    end
+                else
+                    hold on;
+                    size_of_traj = [size_of_traj, length(V{n,m})];
+                    plot(V{n,m}, 'b-.');
+                    segment = V{n,m};
+                    reaction_time = [reaction_time; segment(1:198)'];
+                end
+            end
         end
     end
-    
 end
 
-E = all_BFneu;
-len = size(E);
-for n=1:len(1)
-    for m=1:len(2)
-        if length(E{n,m}) ~= 0
-%             if any(E{n,m} == 1)
-                hold on;
-                size_of_traj = [size_of_traj, length(E{n,m})];
-                plot(E{n,m}, 'b-.');
-                segment = E{n,m};
-                reaction_time = [reaction_time; segment(1:198)'];    
-%             end
-        end
-    end
-    
-end
+%% get min, avg, and max duration of handover
+[Min, ~] = min(size_of_traj);
+Avg = mean(size_of_traj);
+[Max, ~] = max(size_of_traj);
 
-[Min, ind] = min(size_of_traj)
-Avg = mean(size_of_traj)
-[Max, ind] = max(size_of_traj)
-
-Avg_reaction_time = mean(reaction_time);
-std_reaction_time = std(reaction_time);
-
-x = 1:numel(reaction_time(1,:));
-
+%% plot the mean and std of classifications
+size_reaction_time = size(reaction_time);
 % 103 you can assume it is a normal distribution [0, 1]
-% 84% confidence interval
+Avg_reaction_time = mean(reaction_time);
 
+% 84% confidence interval
+% confidence interval for binomial distribution
 std_reaction_time = sqrt((Avg_reaction_time.*(1-Avg_reaction_time))/size_reaction_time(1));
 
-size_reaction_time = size(reaction_time);
-curve1 = Avg_reaction_time + sqrt((Avg_reaction_time.*(1-Avg_reaction_time))/size_reaction_time(1));
-curve2 = Avg_reaction_time - sqrt((Avg_reaction_time.*(1-Avg_reaction_time))/size_reaction_time(1));
+curve1 = Avg_reaction_time + std_reaction_time;
+curve2 = Avg_reaction_time - std_reaction_time;
+
+x = 1:numel(reaction_time(1,:));
 x2 = [x, fliplr(x)];
 inBetween = [curve1, fliplr(curve2)];
 fill(x2, inBetween, 'g','FaceAlpha',0.5);
 hold on;
 plot(x, Avg_reaction_time, 'r', 'LineWidth', 2);
+
 xlim([0, 250]);
 
 %% count the number of 1s in array
@@ -69,10 +69,8 @@ complete_classf(complete_classf~=1)= 0;
 
 complete_classf_inv = reaction_time;
 complete_classf_inv(complete_classf_inv > 0) = -1;
-
 complete_classf_inv(complete_classf_inv == 0) = 1;
 complete_classf_inv(complete_classf_inv==-1) = 0;
-% complete_classf_inv(complete_classf==-1) = 0;
 
 complete_classf_all = sum(complete_classf)/size_reaction_time(1) + sum(complete_classf_inv)/size_reaction_time(1);
 
